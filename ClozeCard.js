3a317d4731e8688bff3f6539e9ac9991ea7e42a6
jasonjoachim <joachim.jason@gmail.com>
2017-09-11 21:34:46 -0500

    cloze questions working, some questions loaded

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,1 +1,17 @@
-// building a cloze card constructor
+// ClozeCard constructor takes two arguments: text and cloze.
+// These values represent the full text and the cloze-deleted portion of the flashcard text.
+exports.ClozeCard = function(text, cloze) {
+	// Convert the incoming strings to lower case
+	var textToLower = text.toLowerCase();
+	var clozeToLower = cloze.toLowerCase();
+
+	// Confirm that the cloze statement appears within the complete text
+	if (!textToLower.includes(clozeToLower)) {
+		console.log('ERROR: cloze-deletion does not appear within full text -- <' + cloze + '>');
+		return;
+	}
+
+	this.full = text;
+	this.cloze = cloze;
+	this.partial = text.replace(cloze, '...');
+}
